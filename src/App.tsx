72690d1b0bfc27fa444a32f4a82d7240ba6657fa
arm1n18 <bachinskijdenis@gmail.com>
2026-03-09 12:36:51 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import { MainPage, VariablesPage, VariablePage } from './pages'
 import { Header } from './components/header/Header.component'
 
 export default function App() {
   return (
     <>
-      <BrowserRouter basename="/vin-decoder">
-        <Header />
-        <div className='container'>
-            <Routes>
-              <Route path="/" element={<MainPage />} />
-              <Route path="/variables" element={<VariablesPage />} />
-              <Route path='/variables/:variableId' element={<VariablePage />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      <Header />
+      <div className='container'>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/variables" element={<VariablesPage />} />
+            <Route path='/variables/:variableId' element={<VariablePage />} />
+        </Routes>
+      </div>
     </>
   )
 }
